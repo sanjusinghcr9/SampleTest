f6121d9e9e2c659f0859e5d6cd9a59b034b921f6
Mr.Singh <Mr.Singh@SANJU-PC>
2016-08-27 09:11:23 +0530

    Add project files.

---
+++ TestingReflection/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestingReflection.MvcApplication" Language="C#" %>
---
+++ TestingReflection/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestingReflection.Startup))]
+namespace TestingReflection
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
